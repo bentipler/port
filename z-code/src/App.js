77e732cae80f1ef30f2878afc11199e881190d21
Benjamin Tipler <ben.j.tipler@gmail.com>
2017-09-30 22:05:56 -0500

    font style updates

--- z-code/src/App.js
+++ z-code/src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Ben Tipler: <span>Lead User Experience Designer</span></h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <span>Hi</span>re me.
         </p>
       </div>
     );
